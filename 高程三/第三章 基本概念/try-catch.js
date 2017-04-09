1a5aa8dde39aca3a4fd547e31ac281212ed14681
zhangolve <zhangolve@gmail.com>
2017-04-09 10:28:55 +0800

    handle error

--- 高程三/第三章 基本概念/try-catch.js
+++ 高程三/第三章 基本概念/try-catch.js
@@ -105,5 +105,28 @@ console.log(err);
 */
 
 
+var  ranError=new RangeError('uprer');
+console.log(ranError.name);
+console.log(ranError.message);
 
 
+
+/*错误类型
+EvalError
+Creates an instance representing an error that occurs regarding the global function eval().
+InternalError 
+Creates an instance representing an error that occurs when an internal error in the JavaScript engine is thrown. E.g. "too much recursion".
+RangeError
+Creates an instance representing an error that occurs when a numeric variable or parameter is outside of its valid range.
+ReferenceError
+Creates an instance representing an error that occurs when de-referencing an invalid reference.
+SyntaxError
+Creates an instance representing a syntax error that occurs while parsing code in eval().
+TypeError
+Creates an instance representing an error that occurs when a variable or parameter is not of a valid type.
+URIError
+Creates an instance representing an error that occurs when encodeURI() or decodeURI() are passed invalid parameters.
+
+
+
+*/
