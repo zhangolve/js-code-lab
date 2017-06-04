2e355f650d7ef74d1f5f3ec5fed27723a9f84407
zhangxingwang <zhangolve@gmail.com>
2017-06-04 16:05:05 +0800

    sort numbers using react

--- React/awesome-react-create-app/src/SortNumber.js
+++ React/awesome-react-create-app/src/SortNumber.js
@@ -1,10 +1,55 @@
-import React, { Component } from 'react';
+/*
+20170604 
+
+本例也是仿照此repo高程三13章有关拖动部分用react的实现
+*/
+import React, {Component} from 'react';
 
 class SortNumber extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numArr: []
+        };
+    }
+
+    
+    componentWillMount() {
+    this.createRandomNumList();
+
+    }
+    
+
+    createRandomNumList = () => {
+        var numArr = [];
+
+        while (true) {
+            var ranNum = Math.round(Math.random() * 10);
+            if (numArr.indexOf(ranNum) === -1 && ranNum !== 0) {
+                numArr.push(ranNum);
+            }
+            if (numArr.length ===10) {
+                console.log(numArr);
+                break;
+            }
+
+        }
+         this.setState({numArr});
+        
+    }
+
     render() {
+        var liHTML="";
+        var {numArr}=this.state;
+        liHTML=  numArr.map( (num,index)=><li key={index}  >{num}</li>  );
         return (
             <div>
                 hello world
+                <ul>
+                {liHTML}
+                </ul>
+            <button onClick={()=>this.createRandomNumList() }  >refresh</button>
+
             </div>
         );
     }
--- React/lift-circle/src/Routes.js
+++ React/lift-circle/src/Routes.js
@@ -1,6 +1,6 @@
 // src/routes.js
 import React from 'react';
-import {  Route,Miss ,Match} from 'react-router';
+import {  Route,Miss } from 'react-router';
 import App from './App';
 import SortNumber from './SortNumber';
 import NotFound from './NotFound';
@@ -8,9 +8,9 @@ import NotFound from './NotFound';
 const Routes = () => (
   <div >
     <Route exact  path="/" component={App} />
-    <Route  path="/sort-numbers" component={SortNumber} />
-    <Route path="*"  component={NotFound}/>
-  </div>
+    <Route path="/sort-numtttbers" component={SortNumber} />
+    
+  </333div>
 );
 
 export default Routes;
