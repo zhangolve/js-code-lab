b4f98d7fb03d9ed3b966d3d7aff3001b741c5189
zhangxingwang <zhangolve@gmail.com>
2017-06-05 19:26:37 +0800

    zquery add some function to a $

--- 高程三/闭包和作用域/create-a-like-jquery-library/zquery.js
+++ 高程三/闭包和作用域/create-a-like-jquery-library/zquery.js
@@ -1,3 +1,36 @@
-(function(global,factory){
+/*这是一个最简单的方式来实现一个给window对象上注入一个属性的方法*/
 
+/*
+原型继承的方式
+    function person(){
+	 
+	}
+	person.prototype.sayName=function(){
+		console.log('zhangolve');
+	}
+	person1=new person(); //这个new的过程在这里又是必不可少的了。
+	person1.sayName();
+
+*/
+//通过使用构造出object的方式来对$增加一些方法
+(function(global,factory){
+    window.$=function(){
+        return {
+            book:function(){
+                console.log('this is a book');
+            },
+            cat:function(){
+                console.log('this is a cat');
+            },
+            copy:function(arr){
+                try {
+                   return  arr.concat(arr)
+                }  
+                catch(e){
+                    console.log(e);
+                }
+            }
+        }
+    }
+   
 })(window,document)
