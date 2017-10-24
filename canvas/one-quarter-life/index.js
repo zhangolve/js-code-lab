743a5daf89814f500ef52090249671df4a281a01
zhangxingwang <zhangolve@gmail.com>
2017-10-24 18:43:30 +0800

    one quarter life for 25th birthday

---
+++ canvas/one-quarter-life/index.js
@@ -0,0 +1,29 @@
+var c=document.getElementById("first-canvas");
+var ctx=c.getContext("2d");
+var cw= c.width=500;
+var ch= c.height=500;
+function forthCircle(){
+
+		ctx.beginPath();
+		ctx.arc(200,200,200,1.5*Math.PI,2*Math.PI,false);
+		ctx.lineTo(200,200);
+		ctx.fillStyle="gray";
+		ctx.fill();
+}
+
+function restCircle(){
+
+		ctx.beginPath();
+		ctx.arc(200,200,200,0,1.5*Math.PI,false);
+		ctx.lineTo(200,200);
+		ctx.fillStyle="red";
+		ctx.fill();
+}
+
+
+forthCircle();
+restCircle();
+
+
+
+
