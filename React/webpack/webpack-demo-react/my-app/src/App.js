423685b48666296ad68b43aaeb832651e211cc3c
zhangxingwang <zhangolve@gmail.com>
2018-05-27 16:58:15 +0800

    cra not resolve tree shaking

---
+++ React/webpack/webpack-demo-react/my-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Header} from './Header.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header>
+            hello world
+        </Header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
