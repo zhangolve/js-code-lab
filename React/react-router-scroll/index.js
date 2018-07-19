548fca58f7d04add6eae73198b57e29e69b51e77
zhangxingwang <zhangolve@gmail.com>
2018-07-19 22:40:17 +0800

    leetcode

---
+++ React/react-router-scroll/index.js
@@ -0,0 +1,4 @@
+// fork from https://github.com/taion/react-router-scroll
+// react-router-scroll for React-router-dom v4
+export { default as ScrollContainer } from './ScrollContainer';
+export { default as ScrollContext } from './ScrollBehaviorContext';
---
+++ 算法/leetcode/71-simplify-path.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path)  {
+    // // replace / one more /
+    // ./ remove
+    // ../ 连同前面一起去掉，如果不是../　否则继续
+    path = path.replace(/\/{2,}/, '/').replace(/\.\//,'');
+    pathArr = path.split('/');
+    console.log(pathArr)
+    newArr = [];
+    for(var i=0;i<pathArr.length;i++) {
+        if(pathArr[i]==='..'){
+            newArr.pop();
+        } else {
+            newArr.push(pathArr[i])
+        }
+    }
+    console.log(newArr)
+    var str = newArr.join('/');
+    var prefix ;
+    if(str.length>1 && str.endsWith('/')){
+        prefix = str.slice(0,-1);
+    } else {
+        prefix = str;
+    }  
+    if(prefix.match(/(\.||\..)$/) && !prefix.match(/(\...)$/)){
+        return prefix.replace(/(\.||\..)$/,'')
+    } else {
+        return prefix
+    }
+};
+
+
+// 一个失败的尝试
+//但是也复习了一些正则表达式的相关知识
+
+//http://blog.sodhanalibrary.com/2015/06/simplify-path-using-javascript.html
+
