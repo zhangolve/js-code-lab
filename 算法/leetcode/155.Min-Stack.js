8f339954e2318f95a07d6e60d44a0a3fb5782377
zhangxingwang <zhangolve@gmail.com>
2018-09-13 11:40:44 +0800

    leetcode 155

---
+++ 算法/leetcode/155.Min-Stack.js
@@ -0,0 +1,56 @@
+/**
+js 模式 
+*/
+
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+    this.stack = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+    this.stack.push(x);   
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length-1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return Math.min(...this.stack);
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+
+var obj = new MinStack();
+obj.push(1)
+obj.pop();
+obj.push(-2)
+obj.push(0)
+obj.push(-3)
+var param_3 = obj.getMin()
+console.log(param_3);
