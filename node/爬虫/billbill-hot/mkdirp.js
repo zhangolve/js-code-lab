63fd99876f5bc483dd9b3506db5e9cab86d8a253
zhangxingwang <zhangolve@gmail.com>
2018-12-11 20:33:50 +0800

    remove test

--- node/爬虫/billbill-hot/mkdirp.js
+++
@@ -1,7 +0,0 @@
-var mkdirp = require('mkdirp');
-mkdirp('./videos/美好故事', function(err) { 
-    console.log('created');
-    console.log(err); //null
-    // path exists unless there was an error
-
-});
