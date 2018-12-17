89749a1ee0ce2578d77cbe5e59b6c22769e102e1
zhangxingwang <zhangolve@gmail.com>
2018-12-17 20:28:53 +0800

    enhance: limit download videos 80

--- node/crawler/billbill-hot/const.js
+++ node/crawler/billbill-hot/const.js
@@ -1,5 +1,5 @@
 config = {
-  url: (authorId) => `https://space.bilibili.com/ajax/member/getSubmitVideos?mid=${authorId}&pagesize=100&tid=0&page=1&keyword=&order=pubdate`,
+  url: (authorId) => `https://space.bilibili.com/ajax/member/getSubmitVideos?mid=${authorId}&pagesize=80&tid=0&page=1&keyword=&order=pubdate`,
   myAuthorId: 2486887,
   headers: {
     'Content-Type': 'application/json',
