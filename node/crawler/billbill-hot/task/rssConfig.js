a253a62a42885aa79fd88d09600c663e78f3f0b1
zhangxingwang <zhangolve@gmail.com>
2018-12-17 21:10:14 +0800

    upload rssconfig

--- node/crawler/billbill-hot/task/rssConfig.js
+++ node/crawler/billbill-hot/task/rssConfig.js
@@ -4,6 +4,8 @@ config = {
    {id: '268522594', playListId: 'PLoHxWFMhISQwyxwmwhGy8eDuj53V8nlUt', playListName:'请不要点开我的收藏夹'},
    {id: '10518076', playListId: 'PLoHxWFMhISQxzj90OUubWs3JTKceDVFjz', playListName:'南国小豆豆'},
    {id: '363212074', playListId: 'PLoHxWFMhISQy1npPfFLCzVpjV8SwSdTDU', playListName:'中国乒乓球'},
+   {id: '331782289', playListId: 'PLoHxWFMhISQx6JEaVYQcLLrqBVyHrT4G7', playListName:'搞笑课代表'},
+   
   ] 
 }
 
