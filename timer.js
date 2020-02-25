e84608e44dab985ad68fd3e33c5586bbba5d39f0
Olive Zhang <xingwang.zhang@everbridge.com>
2020-02-25 17:06:22 +0800

    promise async await demo

---
+++ timer.js
@@ -0,0 +1,73 @@
+const arr = [];
+const abc =(value)=> {
+console.log(value,'value');
+
+// global.intervalTimer = setInterval(()=>{
+//     if (this.timer) {
+//         return;
+//     } else {
+//         this.timer = setTimeout(() => {
+//             arr.push(value)
+//             clearTimeout(this.timer);
+//             console.log(this.intervalTimer)
+//             clearInterval(global.intervalTimer)
+//             global.intervalTimer = null;
+//             this.timer =null;
+//             console.log(arr);
+//         }, Math.random() * 10000);
+//     }
+// },100)
+
+
+
+
+// this.timer = setTimeout(() => {
+//     arr.push(value)
+//     clearTimeout(this.timer);
+//     // clearInterval(intervalTimer)
+//     // this.timer =null;
+//     console.log(arr);
+// }, Math.random() * 10000);
+}
+
+
+let doingDelay = false;
+let interalTimer = null;
+const delay = ()=> {
+    return new Promise((resolve, reject)=> {
+        interalTimer = setInterval(()=> {
+            if(doingDelay) {
+                return;
+            } else {
+                doingDelay = true;
+                setTimeout(()=> {
+                    doingDelay = false;
+                    clearInterval(interalTimer)
+                    resolve();
+                },Math.random() * 3000)
+            }
+        }, 100)
+    })
+}
+
+async function doWork (value) {
+
+    await delay()
+    doingDelay = false;
+    arr.push(value)
+    clearTimeout(this.timer);
+    console.log(this.intervalTimer)
+    clearInterval(global.intervalTimer)
+    global.intervalTimer = null;
+    this.timer =null;
+    console.log(arr);
+}
+
+
+const allDoWork = async () => {
+    for(let i=0;i<5;i++) {
+        await doWork(i);
+    }
+}
+
+allDoWork();
