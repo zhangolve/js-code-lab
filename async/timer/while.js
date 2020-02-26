28161d7589ac9b6ea10bb921a0f51f1d959b67af
Olive Zhang <xingwang.zhang@everbridge.com>
2020-02-26 16:45:39 +0800

    refactor

--- async/timer/while.js
+++ async/timer/while.js
@@ -1,11 +1,9 @@
 const arr = [];
 
 const delay = (value)=> {
-    doingDelay = true;
     return new Promise((resolve, reject)=> {
         
     setTimeout(()=> {
-        doingDelay = false;
         resolve();
     }, (10-value>0? 10-value : 3) * 1000)
     })
@@ -13,20 +11,10 @@ const delay = (value)=> {
 
 
 async function doWork (value, initArr) {
-    // await interval();
     await delay(value);
     initArr.shift();
-    doingDelay = false;
-
     arr.push(value)
-    clearTimeout(this.timer);
-    clearInterval(global.intervalTimer)
-    global.intervalTimer = null;
-    this.timer =null;
-    console.log(arr);
 }
-
-
 
 const a =[];
 let count=0;
@@ -41,37 +29,16 @@ const update = async (count)=>{
     }
 }
 
-const allDoWork = async (arr=[1,2,3,4,5]) => {
-    console.log('all do work', arr)
-    while(arr.length>0) {
-        await doWork(arr[0], arr);
+const allDoWork = async (initArr=[1,2,3,4,5]) => {
+    while(initArr.length>0) {
+        await delay(initArr[0]);
+        const value = initArr.shift();
+        arr.push(value)
+        console.log(arr)
     }
-
-    // what is different with for loop
-    // for(var i=0;i<5;i++) {
-    //     var value=a.shift();
-    //     await doWork(value);
-    // }
 }
 
-// allDoWork();
-
-// while(a.length>0) {
-//     // while(Math.random()>0.5) {
-//         allDoWork();
-//     // }
-// }
-// for(let i=0;i<5;i++) {
-//     var value = a.shift();
-//     allDoWork(value);
-// }
 setInterval(()=>{
     count+=1;
     update(count);
-},1000)
-
-// allDoWork();
-    // [ 5, 4, 3, 2, 1 ]
-
-
-    // allDoWork();
+},1000);
