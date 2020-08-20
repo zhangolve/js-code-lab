0fce5b5249971a7846c6f0f96005ab13bfd0a903
olive zhang <xingwang.zhang@everbridge.com>
2020-08-20 12:36:26 +0800

    leetcode 7 reverse

--- 算法/leetcode/7-reverse-int.js
+++ 算法/leetcode/7-reverse-int.js
@@ -56,6 +56,85 @@ var reverse = function(x) {
 
 };
 
-var result=reverse(1534236469);
-console.log(result);
+// var result=reverse(1534236469);
+// console.log(result);
 
+// 20200820
+
+/*
+Runtime: 136 ms, faster than 11.79% of JavaScript online submissions for Reverse Integer.
+Memory Usage: 40.1 MB, less than 5.03% of JavaScript online submissions for Reverse Integer.
+*/
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse2 = function(x) {
+	function deal(x) {
+	if(x> ((2**31)-1) || x< (-(2**31))){
+		return 0;
+	} else {
+		return x;
+	}
+	}
+	str = x.toString();
+	if(str.length===0) {
+		return x;
+	}
+	let minus = str[0]==='-'?'-':'';
+	let zhengStr = minus ? str.substr(1): str;
+	reverStr = zhengStr.split('').reverse().join('');
+	// reverse 是新添加的，如果没有reverse呢。
+	return deal(parseInt(minus+reverStr));
+};
+
+console.log(reverse2(1534236469))
+
+
+/* 
+
+以上两种解决方案相差无几。我们来看看更优解。
+
+public int reverse(int x) {
+    int rev = 0;
+    while (x != 0) {
+        int pop = x % 10;
+        x /= 10;
+        rev = rev * 10 + pop;
+    }
+    return rev;
+}
+*/
+var reverse3 = function(x) {	
+	let rev=0;
+ 
+	 while(x!==0) {
+		 let pop = x%10; //取余
+		 x=parseInt(x/10);
+		 rev=rev*10+pop;
+		 if(rev> ((2**31)-1) || rev< (-(2**31))) return 0;
+	 }
+	 return rev;
+ };
+/**
+ * 123
+ * 余数为3
+ * x变为12
+ * rev变为3
+ * 
+ * 继续循环
+ * 
+ * 余数变为2
+ * x变为1
+ * rev变为3*10+2 ==32
+ * 
+ * 最后一轮循环
+ * 
+ * 余数变为1
+ * x变为0
+ * rev
+ * 变为32*10+1=321
+ * 
+ * 循环结束 
+ * 
+ */
