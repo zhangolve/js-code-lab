7bd583092074e2495e9b625baf033c2ae8548fa9
Olive Zhang <xingwang.zhang@everbridge.com>
2020-08-21 15:20:15 +0800

    leetcode 13 roman to int

---
+++ 算法/leetcode/13-roman-to-int.js
@@ -0,0 +1,36 @@
+const obj = {"I":1,"V":5,"X" :10,"L" :50,"C": 100,"D" :500,"M" :1000}
+const spe = ['I','X','C'];
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let int=0;
+    const arr= s.split('');
+    while(arr.length>0) {
+        let roman = arr.shift();
+        // 到达这里的，都是已经过滤掉了CM这样的。
+        if(spe.includes(roman)) {
+            let roman2=   arr.slice(0,1) //arr.shift();
+            let int1 = obj[roman];
+            let int2 = obj[roman2];
+
+            // IV
+            if(int1<int2) {
+                arr.shift();
+                int+=int2-int1;
+            } else {
+                int+=int1;
+            }
+        } else {
+            int+= obj[roman];
+        }
+    }
+    return int;
+};
+
+
+console.log(romanToInt("IV"))
+
+
+// 还真是越来越简单了呢。。
