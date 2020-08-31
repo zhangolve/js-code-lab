5964254ea70707947d03d0b251480b021a8c842e
Olive Zhang <xingwang.zhang@everbridge.com>
2020-08-31 14:35:49 +0800

    climbing stairs

--- 算法/leetcode/70-climbing-stairs.js
+++ 算法/leetcode/70-climbing-stairs.js
@@ -94,8 +94,34 @@ var climbStairs = function(n) {
 
 // (1/√5)*{[(1+√5)/2]^n - [(1-√5)/2]^n}
 
-
+/**
+ *  n=4
+ *  = f(3) + f(2)
+ *  n=3 ,, f(1) + f(2) ，往后倒退一步，或者往后倒退两步。
+ * n=2, 
+ * 
+ * 例如，n=3这种情况，我们就考虑，不从下往上爬台阶，而是从上往下落台阶。这样的话，我们考虑仍然有两种降落的方法，一种是降低一步，一种是降低两步。然后，
+ * 我们假设我们已经知道了降低一步的f(2),以及降低两步的f(1). 这个时候再看，刚才已经说了，降低两步是一个固定的走法，已经确定。相当于f(1)*1,所以最后我们总结出了f(n)=f(n-1)+f(n-2)
+ * 所以，这次我们不用arguments进行求解呢？
+ * 注意在这里，特意将这些得到的f(n) 放到obj中，也是为了下次用的时候，不用再进行计算，降低了时间复杂度。
+ */
 console.log(climbStairs(4))
 
 // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Functions/arguments/callee
 
+var obj ={1:1,2:2}
+var climbStairs = function(n) {
+  if (n===1) {
+      return 1
+  }
+  if (n===2) {
+      return 2;
+  }
+  // 如果n不在obj之中
+  if (!obj[n]) {
+    obj[n] =  climbStairs(n-2)  + climbStairs(n-1) ;
+  }
+  return obj[n]
+};
+
+/*这里就完全没有使用arguments*/
