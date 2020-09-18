fec079d2367ae6e9851a00be02c1eea14e0b5904
olive zhang <xingwang.zhang@everbridge.com>
2020-09-18 16:03:25 +0800

    start vue

---
+++ node/script/guahao.js
@@ -0,0 +1,18 @@
+// https://www.114yygh.com/web/product/list?_time=1600412648012
+// "firstDeptCode: "df36d93f187b96af86dc27ae84a05bb7"
+// hosCode: "142"
+// secondDeptCode: "200048244"
+// week: 1"
+
+// Cookie: hyde_session=AsYDxxYNTVuHlyC0MiEmpFB0DvACk3zk_5334707; hyde_session=AsYDxxYNTVuHlyC0MiEmpFB0DvACk3zk_5334707; __jsluid_s=10d856969c930db335b7fb92ee340b72; secure-key=5918d8a2-6373-4924-acc4-d11c9d47f7ee; hyde_session=AsYDxxYNTVuHlyC0MiEmpFB0DvACk3zk_5334707; cmi-user-ticket=xpaTyQCUMhdDXsszvShZuGEY_gyeqp6CkaHN-w..; hyde_session_tm=1600412603821Cookie: hyde_session=AsYDxxYNTVuHlyC0MiEmpFB0DvACk3zk_5334707; hyde_session=AsYDxxYNTVuHlyC0MiEmpFB0DvACk3zk_5334707; __jsluid_s=10d856969c930db335b7fb92ee340b72; secure-key=5918d8a2-6373-4924-acc4-d11c9d47f7ee; hyde_session=AsYDxxYNTVuHlyC0MiEmpFB0DvACk3zk_5334707; cmi-user-ticket=xpaTyQCUMhdDXsszvShZuGEY_gyeqp6CkaHN-w..; hyde_session_tm=1600412603821
+
+// 请求接口，如果有状态为可以挂的，则邮件或短信提醒。
+
+// https://www.114yygh.com/web/product/list?_time=1600413185533
+
+// firstDeptCode: "ae032b57b80fc160aa7e8a66639b33ee"
+// hosCode: "114"
+// secondDeptCode: "200000781"
+// week: 2
+
+axios 
