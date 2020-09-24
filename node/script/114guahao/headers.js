fbbee38392564afe3de904ec5a761100f623b7a6
Olive Zhang <xingwang.zhang@everbridge.com>
2020-09-25 06:56:25 +0800

    114

--- node/script/114guahao/headers.js
+++ node/script/114guahao/headers.js
@@ -1,6 +1,6 @@
 const headers = {
     // 'Connection': 'keep-alive',
-    Cookie: 'hyde_session=dJg9QpJHyqaGdOg3RzZXycihMnjSDNyw_5336175; hyde_session=F4cHNLlqRrRQX2rrEDBP2YUEY8bVMcAt_5336169; __jsluid_s=ef7f42892567088a95d96af7e73ddbc6; __jsluid_h=cef61cfe56320e14fd8c6a7d7b20678f; cmi-user-ticket=VtYaYIR_am6l393meCzNswq6LJdu0jKtBigYDw..; secure-key=bf6f9d2d-e162-41e1-8858-1d83110d7f0d; hyde_session=dJg9QpJHyqaGdOg3RzZXycihMnjSDNyw_5336175;',
+    Cookie: 'hyde_session=8Mw1ySU1RbyhHsGD5478gCO6v19C0nwz_5336499; hyde_session=vPmWvUmL827sKJDw2HcczqIj0LJsLjdX_5336626; __jsluid_s=ef7f42892567088a95d96af7e73ddbc6; __jsluid_h=cef61cfe56320e14fd8c6a7d7b20678f; secure-key=bb05e13d-0d6c-4714-be47-8f396c89958b; hyde_session=vPmWvUmL827sKJDw2HcczqIj0LJsLjdX_5336626; cmi-user-ticket=pxU6WaPq_mLKfAmR2lGytPaco5UfSkV9aqk0aA..;',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
     "Host": "www.114yygh.com",
 "Referer": "https://www.114yygh.com/hospital/114/ae032b57b80fc160aa7e8a66639b33ee/200000781/source",
--- node/script/114guahao/index.js
+++ node/script/114guahao/index.js
@@ -2,6 +2,17 @@ const axios = require("axios");
 const headers = require('./headers')
 const url = 'https://www.114yygh.com/web/product/list';
 
+const log4js = require('log4js');
+log4js.configure({
+  appenders: { 
+      abc: { type: 'file', filename: 'abc.log', maxLogSize: 10485760, backups: 3, compress: true } ,
+      console: { type: 'console' }
+    }, // 日志的名称
+  categories: { default: { appenders: ['abc','console'], level: 'all' } } // appender 决定了每一行日志前缀是啥
+});
+
+const logger = log4js.getLogger('abc');
+
 async function execuate() {
     try {
        const timeurl =  `${url}?_time=${+new Date}`;
@@ -23,11 +34,28 @@ async function execuate() {
                 week: 1
           }
       });
-      console.log(response.data)
+      logger.info(response.data)
+      const result = response.data
+      const {data} = result;
+      if(data) {
+        const {calendars} = data; 
+        for(var i=0;i<calendars.length;i++) {
+          //{ dutyDate: '2020-09-29', weekDesc: '周二', status: 'SOLD_OUT' }
+          // { dutyDate: '2020-09-30', weekDesc: '周三', status: 'SOLD_OUT' }
+          const calendar = calendars[i];
+          if( (calendar==='2020-09-29' || calendar==='2020-09-30' ) &&  (status !== 'SOLD_OUT' || status !=='NO_INVENTORY' ) ) {
+            axios.get('https://sc.ftqq.com/SCU114867T88e5ff605254c1d9b7937eac8c596fd45f6c9299aed6e.send?text=youhaola');
+          }
+        }
+      } else {
+        axios.get('https://sc.ftqq.com/SCU114867T88e5ff605254c1d9b7937eac8c596fd45f6c9299aed6e.send?text=yichangla');
+      }
     }
     finally {
         console.log('000')
     }
 }
 
-execuate();
+setInterval(()=>{
+  execuate();
+},1000);
