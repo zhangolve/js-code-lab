7328a76b1ff50ce363662c46d55fa439baacb494
zhangolve <zhangolve@gmail.com>
2021-03-13 12:27:54 +0800

    upload youtube

--- node/crawler/ximalaya/upload/upload.py
+++ node/crawler/ximalaya/upload/upload.py
@@ -1,8 +1,12 @@
 from youtube_uploader_selenium import YouTubeUploader
 
-video_path = ' /mnt/c/Users/13823/Music/mp4/人生/mp4s/人生01.mp4'
+video_path = './01.mp4'
 # metadata_path = '123/rockets_metadata.json'
 
 uploader = YouTubeUploader(video_path)
+
+## need to do : for in loop and windows path handler
+
+# if could do better , we can deploy it to docker 
 was_video_uploaded, video_id = uploader.upload()
 assert was_video_uploaded
